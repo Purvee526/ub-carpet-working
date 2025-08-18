9056a26c40cd5b27123b9b4ba546c0ed1ce27642
Your Name <your_email@example.com>
2025-08-17 20:51:35 -0700

    0818

--- src/data.js
+++ src/data.js
@@ -11,12 +11,22 @@ import carpetAB848_7 from './assets/AB848-300x400t.jpg';
 import carpetAB848_8 from './assets/AB848-300x500.jpg';
 import carpetAB848_9 from './assets/AB848-300x500r.jpg';
 import carpetAB848_10 from './assets/AB848-300x500t.jpg';
-
 // 2-р хивсний зургууд
 import carpetAB2005_1 from './assets/AB2005-2x3.jpg';
 import carpetAB2005_2 from './assets/AB2005-300x400m.jpg';
 import carpetAB2005_3 from './assets/AB2005-300x400sh.jpg';
 import carpetAB2005_4 from './assets/AB2005-300x500m.jpg';
+// 3-р хивсний зургууд
+import carpetAB707_1 from './assets/AB707-300x400.jpg';
+import carpetAB707_2 from './assets/AB707-300x400g.jpg';
+// 4-р хивсний зургууд
+import carpetAB749_1 from './assets/AB749-200x300.jpg';
+import carpetAB749_2 from './assets/AB749-255x350.jpg';
+import carpetAB749_3 from './assets/AB749-255x350sh.jpg';
+import carpetAB749_4 from './assets/AB749-300x400.jpg';
+import carpetAB749_5 from './assets/AB749-300x400g.jpg';
+import carpetAB749_6 from './assets/AB749-300x500.jpg';
+import carpetAB749_7 from './assets/AB749-300x500t.jpg';
 
 export const productsData = [
   {
@@ -53,5 +63,34 @@ export const productsData = [
     ],
     sizes: ['2x3', '300x400', '300x500'],
     colors: ['#216F80', '#000080', '#D9A943'],
+  },
+  {
+    id: 3,
+    brand: 'АЛТАНБУЛАГ ХИВС',
+    code: 'AB707',
+    type: 'классик',
+    images: [
+      { src: carpetAB707_1, size: '300x400', color: '#000080'},
+      { src: carpetAB707_2, size: '300x400', color: '#b8b8b8ff' },
+    ],
+    sizes: ['300x400'],
+    colors: ['#000080', '#b8b8b8ff'],
+  },
+  {
+    id: 4,
+    brand: 'АЛТАНБУЛАГ ХИВС',
+    code: 'AB749',
+    type: 'классик',
+    images: [
+      { src: carpetAB749_1, size: '200x300', color: '#b8b8b8ff' },
+      { src: carpetAB749_2, size: '255x350', color: '#216F80' },
+      { src: carpetAB749_3, size: '255x350', color: '#b8b8b8ff' },
+      { src: carpetAB749_4, size: '300x400', color: '#216F80' },
+      { src: carpetAB749_5, size: '300x400', color: '#b8b8b8ff' },
+      { src: carpetAB749_6, size: '300x500', color: '#216F80'},
+      { src: carpetAB749_7, size: '300x500', color: '#b8b8b8ff'},
+    ],
+    sizes: ['200x300', '255x350', '300x400', '300x500'],
+    colors: ['#b8b8b8ff', '#216F80'],
   },
 ];
