e93114e82c46fa5cde6aa88cf66e790014bc03cb
Your Name <your_email@example.com>
2025-10-19 18:15:44 -1200

    qr ok 1020

--- src/components/make_qr.py
+++ src/components/make_qr.py
@@ -3,7 +3,7 @@ import qrcode
 from PIL import Image
 
 # QR кодын үндсэн мэдээлэл
-url = "https://sales.ubcarpet.mn//"
+url = "https://sales.ubcarpet.mn/"
 # QR кодын тохиргоо
 qr = qrcode.QRCode(
     version=1,
@@ -34,4 +34,4 @@ logo_pos = ((qr_width - logo_size) // 2, (qr_height - logo_size) // 2)
 qr_img.paste(logo, logo_pos, mask=logo if logo.mode == 'RGBA' else None)
 
 # QR кодыг хадгалах
-qr_img.save("catalog.sales.ubcarpet.png")
+qr_img.save("catalog.salesubcarpet.png")
