01a0e0ca6cc58c28e55ba21cfe3e9424e1ccb4bc
Your Name <your_email@example.com>
2025-10-19 16:51:45 -1200

    QR edit 1020

--- src/components/make_qr.py
+++ src/components/make_qr.py
@@ -3,8 +3,7 @@ import qrcode
 from PIL import Image
 
 # QR кодын үндсэн мэдээлэл
-url = "https://ub-carpet-working.vercel.app/"
-
+url = "https://sales.ubcarpet.mn//"
 # QR кодын тохиргоо
 qr = qrcode.QRCode(
     version=1,
@@ -35,4 +34,4 @@ logo_pos = ((qr_width - logo_size) // 2, (qr_height - logo_size) // 2)
 qr_img.paste(logo, logo_pos, mask=logo if logo.mode == 'RGBA' else None)
 
 # QR кодыг хадгалах
-qr_img.save("ub-carpet-working_qr.png")
+qr_img.save("catalog.sales.ubcarpet.png")
